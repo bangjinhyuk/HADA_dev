19c558b1c83f19ca29d532de2e22bc029b31491b
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-16 01:25:33 +0900

    Add MPAndroidChart to DetailActivity

--- FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
+++ FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
@@ -3,11 +3,25 @@ package com.hada.fridgeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
     private TextView detail_title;
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +32,53 @@ public class DetailActivity extends AppCompatActivity {
         Intent getintent = getIntent();
 
         detail_title.setText(getintent.getExtras().getString("title"));
+
+        lineChart = (LineChart)findViewById(R.id.chart);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(1, 1));
+        entries.add(new Entry(2, 2));
+        entries.add(new Entry(3, 0));
+        entries.add(new Entry(4, 4));
+        entries.add(new Entry(5, 3));
+        LineDataSet lineDataSet = new LineDataSet(entries, "속성명1");
+        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//        lineDataSet.setLineWidth(2);
+//        lineDataSet.setCircleRadius(6);
+//        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+//        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+//        lineDataSet.setDrawCircleHole(false);
+//        lineDataSet.setDrawCircles(false);
+//        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+//        lineDataSet.setDrawHighlightIndicators(true);
+        lineDataSet.setDrawValues(false);
+        lineDataSet.setCubicIntensity(0.2f);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setLineWidth(1.8f);
+        lineDataSet.setCircleRadius(4f);
+        lineDataSet.setCircleColor(Color.BLUE);
+        lineDataSet.setHighLightColor(Color.rgb(244, 117, 117));
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setFillColor(Color.BLUE);
+        lineDataSet.setFillAlpha(100);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(8, 24, 0);
+        YAxis yLAxis = lineChart.getAxisLeft();
+        yLAxis.setTextColor(Color.BLACK);
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+        Description description = new Description();
+        description.setText("");
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.invalidate();
     }
 }
