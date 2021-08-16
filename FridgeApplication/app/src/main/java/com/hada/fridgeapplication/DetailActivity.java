1774bd9c85e56d078a54c1f1071d4d83c0037909
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-16 23:00:23 +0900

    Update DetailActivity, Seperate GraphService

--- FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
+++ FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
@@ -22,63 +24,122 @@ import java.util.List;
 public class DetailActivity extends AppCompatActivity {
     private TextView detail_title;
     private LineChart lineChart;
-
+    private ImageView detail_tempbt, detail_humibt;
+    private boolean detailbt = true;
+    private String TAG = "DetailActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         detail_title = findViewById(R.id.detail_title);
+        detail_tempbt = findViewById(R.id.detail_tempbt);
+        detail_humibt = findViewById(R.id.detail_humibt);
+
 
         Intent getintent = getIntent();
-
         detail_title.setText(getintent.getExtras().getString("title"));
 
         lineChart = (LineChart)findViewById(R.id.chart);
+
+        GraphService graphService = new GraphService(lineChart);
+
+        lineChart.getLegend().setEnabled(false);
+
         List<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 1));
         entries.add(new Entry(1, 1));
         entries.add(new Entry(2, 2));
         entries.add(new Entry(3, 0));
         entries.add(new Entry(4, 4));
         entries.add(new Entry(5, 3));
-        LineDataSet lineDataSet = new LineDataSet(entries, "속성명1");
-        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
-//        lineDataSet.setLineWidth(2);
-//        lineDataSet.setCircleRadius(6);
-//        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
-//        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
-//        lineDataSet.setDrawCircleHole(false);
-//        lineDataSet.setDrawCircles(false);
-//        lineDataSet.setDrawHorizontalHighlightIndicator(false);
-//        lineDataSet.setDrawHighlightIndicators(true);
-        lineDataSet.setDrawValues(false);
-        lineDataSet.setCubicIntensity(0.2f);
-        lineDataSet.setDrawFilled(true);
-        lineDataSet.setDrawCircles(false);
-        lineDataSet.setLineWidth(1.8f);
-        lineDataSet.setCircleRadius(4f);
-        lineDataSet.setCircleColor(Color.BLUE);
-        lineDataSet.setHighLightColor(Color.rgb(244, 117, 117));
-        lineDataSet.setColor(Color.BLUE);
-        lineDataSet.setFillColor(Color.BLUE);
-        lineDataSet.setFillAlpha(100);
-        lineDataSet.setDrawHorizontalHighlightIndicator(false);
-        LineData lineData = new LineData(lineDataSet);
-        lineChart.setData(lineData);
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setTextColor(Color.BLACK);
-        xAxis.enableGridDashedLine(8, 24, 0);
-        YAxis yLAxis = lineChart.getAxisLeft();
-        yLAxis.setTextColor(Color.BLACK);
-        YAxis yRAxis = lineChart.getAxisRight();
-        yRAxis.setDrawLabels(false);
-        yRAxis.setDrawAxisLine(false);
-        yRAxis.setDrawGridLines(false);
-        Description description = new Description();
-        description.setText("");
-        lineChart.setDoubleTapToZoomEnabled(false);
-        lineChart.setDrawGridBackground(false);
-        lineChart.setDescription(description);
-        lineChart.invalidate();
+        entries.add(new Entry(6, 3));
+        entries.add(new Entry(7, 3));
+        entries.add(new Entry(8, 3));
+        entries.add(new Entry(9, 3));
+        entries.add(new Entry(10, 3));
+        entries.add(new Entry(11, 3));
+        entries.add(new Entry(12, 3));
+        entries.add(new Entry(13, 3));
+        entries.add(new Entry(14, 3));
+        entries.add(new Entry(15, 3));
+        entries.add(new Entry(16, 2));
+        entries.add(new Entry(17, 1));
+        entries.add(new Entry(18, 0));
+        entries.add(new Entry(19, 3));
+        entries.add(new Entry(20, 4));
+        entries.add(new Entry(21, 5));
+        entries.add(new Entry(22, 6));
+        entries.add(new Entry(23, 7));
+
+        List<Entry> entries1 = new ArrayList<>();
+        entries1.add(new Entry(0, 23));
+        entries1.add(new Entry(1, 23));
+        entries1.add(new Entry(2, 25));
+        entries1.add(new Entry(3, 06));
+        entries1.add(new Entry(4, 46));
+        entries1.add(new Entry(5, 37));
+        entries1.add(new Entry(6, 34));
+        entries1.add(new Entry(7, 32));
+        entries1.add(new Entry(8, 31));
+        entries1.add(new Entry(9, 33));
+        entries1.add(new Entry(10, 36));
+        entries1.add(new Entry(11, 39));
+        entries1.add(new Entry(12, 36));
+        entries1.add(new Entry(13, 35));
+        entries1.add(new Entry(14, 34));
+        entries1.add(new Entry(15, 33));
+        entries1.add(new Entry(16, 32));
+        entries1.add(new Entry(17, 31));
+        entries1.add(new Entry(18, 33));
+        entries1.add(new Entry(19, 34));
+        entries1.add(new Entry(20, 35));
+        entries1.add(new Entry(21, 37));
+        entries1.add(new Entry(22, 38));
+        entries1.add(new Entry(23, 37));
+
+        if (detailbt) {
+            graphService.drawGraph(entries,Color.rgb(153, 153, 255));
+            detail_tempbt.setImageResource(R.drawable.click_temp_tab);
+            detail_humibt.setImageResource(R.drawable.humi_tab);
+        }else{
+//            graphService.drawGraph(entries1,Color.rgb(162, 225, 199),lineChart);
+            detail_tempbt.setImageResource(R.drawable.temp_tab);
+            detail_humibt.setImageResource(R.drawable.click_humi_tab);
+        }
+
+        /*
+         * 온도 측정 기록 탭 클릭시
+         */
+        detail_tempbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!detailbt) {
+                    graphService.drawGraph(entries,Color.rgb(153, 153, 255));
+                    detail_tempbt.setImageResource(R.drawable.click_temp_tab);
+                    detail_humibt.setImageResource(R.drawable.humi_tab);
+                    detailbt = true;
+
+                }
+            }
+        });
+        /*
+         * 습도 측정 기록 탭 클릭시
+         */
+        detail_humibt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (detailbt) {
+                    graphService.drawGraph(entries1,Color.rgb(162, 225, 199));
+                    detail_tempbt.setImageResource(R.drawable.temp_tab);
+                    detail_humibt.setImageResource(R.drawable.click_humi_tab);
+                    detailbt = false;
+                }
+            }
+        });
+
+
+
+
     }
 }
---
+++ FridgeApplication/app/src/main/java/com/hada/fridgeapplication/GraphService.java
@@ -0,0 +1,76 @@
+package com.hada.fridgeapplication;
+
+import android.graphics.Color;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphService {
+
+    LineChart lineChart;
+
+    public GraphService(LineChart lineChart) {
+        this.lineChart = lineChart;
+    }
+
+    public void drawGraph(List<Entry> entries, int color){
+        LineDataSet lineDataSet = new LineDataSet(entries, "");
+        lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+//        lineDataSet.setLineWidth(2);
+//        lineDataSet.setCircleRadius(6);
+//        lineDataSet.setCircleColor(Color.parseColor("#FFA1B4DC"));
+//        lineDataSet.setColor(Color.parseColor("#FFA1B4DC"));
+//        lineDataSet.setDrawCircleHole(false);
+//        lineDataSet.setDrawCircles(false);
+        lineDataSet.setDrawHighlightIndicators(true);
+        lineDataSet.setDrawValues(true);
+        lineDataSet.setCubicIntensity(0.3f);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setDrawCircles(false);
+        lineDataSet.setLineWidth(0.6f);
+//        lineDataSet.setCircleRadius(4f);
+//        lineDataSet.setCircleColor(Color.BLUE);
+//        lineDataSet.setHighLightColor(Color.rgb(244, 117, 117));
+        lineDataSet.setColor(color);
+        lineDataSet.setFillColor(color);
+        lineDataSet.setFillAlpha(100);
+        lineDataSet.setDrawHorizontalHighlightIndicator(false);
+        lineDataSet.setDrawVerticalHighlightIndicator(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.TOP);
+        xAxis.setTextColor(Color.BLACK);
+        xAxis.enableGridDashedLine(8, 10, 0);
+
+        YAxis yLAxis = lineChart.getAxisLeft();
+//        yLAxis.setTextColor(Color.BLACK);
+        yLAxis.setDrawLabels(false);
+        yLAxis.setDrawAxisLine(false);
+        yLAxis.setDrawGridLines(false);
+        YAxis yRAxis = lineChart.getAxisRight();
+        yRAxis.setDrawLabels(false);
+        yRAxis.setDrawAxisLine(false);
+        yRAxis.setDrawGridLines(false);
+
+
+        Description description = new Description();
+        description.setText("");
+
+        lineChart.setDoubleTapToZoomEnabled(false);
+        lineChart.setDrawGridBackground(false);
+        lineChart.setDescription(description);
+        lineChart.invalidate();
+    }
+
+
+}
