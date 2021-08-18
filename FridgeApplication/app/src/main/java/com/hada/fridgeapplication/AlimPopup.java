aa441b2415cb72a46e0a8ce3588ec090aa75c311
bangjinhyuk <jinhyuk0103@gmail.com>
2021-08-19 01:37:32 +0900

    Update AlimPopup, RangeSeekbar, DetailActivity, BatteryLevel

--- FridgeApplication/app/src/main/java/com/hada/fridgeapplication/AlimPopup.java
+++ FridgeApplication/app/src/main/java/com/hada/fridgeapplication/AlimPopup.java
@@ -3,7 +3,10 @@ package com.hada.fridgeapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.widget.TextView;
 
@@ -14,6 +17,8 @@ public class AlimPopup extends Activity {
 
     CrystalRangeSeekbar range_temp,range_humi;
     TextView temperature_popup, humidity_popup;
+    private int temp_minValue,temp_maxValue,humi_minValue,humi_maxValue;
+    private String TAG= "AlimPopup";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,25 @@ public class AlimPopup extends Activity {
         temperature_popup = findViewById(R.id.temperature_popup);
         humidity_popup = findViewById(R.id.humidity_popup);
 
+        Intent intent = getIntent();
+//        Log.d(TAG, "onCreate: temp_minValue "+intent.getExtras().getInt("temp_minValue",0));
+//        Log.d(TAG, "onCreate: temp_maxValue "+intent.getExtras().getInt("temp_maxValue",0));
+//        Log.d(TAG, "onCreate: humi_minValue "+intent.getExtras().getInt("humi_minValue",0));
+//        Log.d(TAG, "onCreate: humi_maxValue "+intent.getExtras().getInt("humi_maxValue",0));
+
+        range_temp.setMinStartValue(intent.getExtras().getInt("temp_minValue",0))
+                .setMaxStartValue(intent.getExtras().getInt("temp_maxValue",0))
+                .apply();
+        range_humi.setMinStartValue(intent.getExtras().getInt("humi_minValue",0))
+                .setMaxStartValue(intent.getExtras().getInt("humi_maxValue",0))
+                .apply();
+
+
         range_temp.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
             @Override
             public void valueChanged(Number minValue, Number maxValue) {
+                temp_minValue = Integer.parseInt(String.valueOf(minValue));
+                temp_maxValue = Integer.parseInt(String.valueOf(maxValue));;
                 temperature_popup.setText(minValue+"℃ ~ "+maxValue+"℃");
             }
         });
@@ -36,8 +57,24 @@ public class AlimPopup extends Activity {
         range_humi.setOnRangeSeekbarChangeListener(new OnRangeSeekbarChangeListener() {
             @Override
             public void valueChanged(Number minValue, Number maxValue) {
+                humi_minValue = Integer.parseInt(String.valueOf(minValue));
+                humi_maxValue = Integer.parseInt(String.valueOf(maxValue));;
                 humidity_popup.setText(minValue+"% ~ "+maxValue+"%");
             }
         });
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+//        Log.d("dispatchTouchEvent", "dispatchTouchEvent: ");
+        Intent intent = new Intent(getApplicationContext(), DetailActivity.class)
+                .putExtra("temp_minValue", temp_minValue)
+                .putExtra("temp_maxValue", temp_maxValue)
+                .putExtra("humi_minValue", humi_minValue)
+                .putExtra("humi_maxValue", humi_maxValue);
+        setResult(RESULT_OK, intent);
+
+        return super.dispatchTouchEvent(ev);
+    }
+
 }
--- FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
+++ FridgeApplication/app/src/main/java/com/hada/fridgeapplication/DetailActivity.java
@@ -1,14 +1,22 @@
 package com.hada.fridgeapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Description;
@@ -24,9 +32,11 @@ import java.util.List;
 public class DetailActivity extends AppCompatActivity {
     private TextView detail_title;
     private LineChart lineChart;
-    private ImageView detail_tempbt, detail_humibt, detail_alim;
+    private ImageView detail_tempbt, detail_humibt, detail_alim, detail_battery, detail_modify_title, detail_back;
     private boolean detailbt = true;
     private String TAG = "DetailActivity";
+    private ActivityResultLauncher<Intent> resultLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +46,9 @@ public class DetailActivity extends AppCompatActivity {
         detail_tempbt = findViewById(R.id.detail_tempbt);
         detail_humibt = findViewById(R.id.detail_humibt);
         detail_alim = findViewById(R.id.detail_alim);
+        detail_battery = findViewById(R.id.detail_battery);
+        detail_modify_title = findViewById(R.id.detail_modify_title);
+        detail_back = findViewById(R.id.detail_back);
 
         Intent getintent = getIntent();
         detail_title.setText(getintent.getExtras().getString("title"));
@@ -46,6 +59,65 @@ public class DetailActivity extends AppCompatActivity {
 
         lineChart.getLegend().setEnabled(false);
 
+        //Todo: 뒤로가기 버튼 이벤트 처리
+        detail_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SensorListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //Todo: 냉장고 이름 수정 버튼 처리
+        detail_modify_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),ModifyPopup.class);
+                startActivity(intent);
+            }
+        });
+
+
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG, "onActivityResult: resultLauncher");
+                        if(result.getResultCode() == RESULT_OK){
+                            Log.d(TAG, "onActivityResult: resultLauncher ok");
+                            //Todo: 받아온 값을 서버에 등록
+                            Log.d(TAG, "onActivityResult: temp_minValue "+result.getData().getExtras().getInt("temp_minValue",0));
+                            Log.d(TAG, "onActivityResult: temp_maxValue "+result.getData().getExtras().getInt("temp_maxValue",0));
+                            Log.d(TAG, "onActivityResult: humi_minValue "+result.getData().getExtras().getInt("humi_minValue",0));
+                            Log.d(TAG, "onActivityResult: humi_maxValue "+result.getData().getExtras().getInt("humi_maxValue",0));
+                        }
+                    }
+                });
+
+        //Todo: 알림 설정 버튼 처리
+        detail_alim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), AlimPopup.class);
+                intent.putExtra("temp_minValue", -12);
+                intent.putExtra("temp_maxValue", 12);
+                intent.putExtra("humi_minValue", 12);
+                intent.putExtra("humi_maxValue", 24);
+                resultLauncher.launch(intent);
+            }
+        });
+
+        //Todo: 배터리 잔량 파악
+        int batteryLevel = getBatteryState(getApplicationContext());
+        Log.d(TAG, "onCreate: battery level = "+batteryLevel);
+        if (batteryLevel>80) detail_battery.setImageResource(R.drawable.battery_high);
+        else if (batteryLevel>40) detail_battery.setImageResource(R.drawable.battery_normal);
+        else if (batteryLevel>10) detail_battery.setImageResource(R.drawable.battery_low);
+        else detail_battery.setImageResource(R.drawable.battery_empty);
+
+        //Todo: 서버에서 일일 측정 온도 받아오는 부분
         List<Entry> entries = new ArrayList<>();
         entries.add(new Entry(0, 1));
         entries.add(new Entry(1, 1));
@@ -138,17 +210,33 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-        detail_alim.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(v.getContext(), AlimPopup.class);
-                intent.putExtra("data", "Test Popup");
-                v.getContext().startActivity(intent);
-            }
-        });
 
 
 
 
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Log.d(TAG, "onActivityResult: ");
+//        if(requestCode==0){
+//            if (resultCode==RESULT_OK) {
+//                Toast.makeText(DetailActivity.this, "result ok!", Toast.LENGTH_SHORT).show();
+//            }else{
+//                Toast.makeText(DetailActivity.this, "result cancle!", Toast.LENGTH_SHORT).show();
+//            }
+//        }else if(requestCode==1){
+//        }
+//    }
+
+
+    public int getBatteryState(Context context) {
+        Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level / (float)scale;
+        return (int)(batteryPct * 100);
     }
 }
