a021cffc4907c0eb06718bb8ddccfe604c21c06d
bangjinhyuk <jinhyuk0103@gmail.com>
2021-09-28 20:31:42 +0900

    feat:: SQLite DB생성 및 약 알림 정보 저장

--- PillManagement/app/src/main/java/com/hada/pillmanagement/CalendarActivity.java
+++ PillManagement/app/src/main/java/com/hada/pillmanagement/CalendarActivity.java
@@ -7,7 +7,10 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,6 +29,12 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        Database database;
+        SQLiteDatabase db;
+        database = new Database(CalendarActivity.this, "pill.db", null, 1);
+        db = database.getWritableDatabase();
+        database.onCreate(db);
+
         calendarView = findViewById(R.id.calendar_view);
         plus = findViewById(R.id.bt_plus);
         minus = findViewById(R.id.bt_minus);
@@ -43,11 +52,30 @@ public class CalendarActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if(result.getResultCode() == RESULT_OK){
+                            ContentValues values = new ContentValues();
+                            values.put("name",result.getData().getStringExtra("name"));
+                            values.put("day",result.getData().getStringExtra("day"));
+                            values.put("setHour",result.getData().getIntExtra("setHour",0));
+                            values.put("setMin",result.getData().getIntExtra("setMin",0));
+                            values.put("week",result.getData().getStringExtra("week"));
+
+                            db.insert("mytable",null,values);
+
+                            System.out.println("===============");
+
+                            Cursor c = db.query("mytable",null,null,null,null,null,null,null);
+                            while(c.moveToNext()){
+                                System.out.println("name : "+c.getString(c.getColumnIndex("name")));
+                            }
+                            System.out.println("===============");
+
                             System.out.println(result.getData().getStringExtra("name"));
                             System.out.println(result.getData().getStringExtra("day"));
                             System.out.println(result.getData().getIntExtra("setHour",0));
                             System.out.println(result.getData().getIntExtra("setMin",0));
                             System.out.println(result.getData().getStringExtra("week"));
+
+
                         }
                     }
                 });
